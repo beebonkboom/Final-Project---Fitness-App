9c8e91466ef301b768b1059695c25ee66742a19c
xanyu12 <112962236+xanyu12@users.noreply.github.com>
2024-04-08 12:40:09 -0400

    Add files via upload

---
+++ SQL/Populate (1).sql
@@ -0,0 +1,63 @@
+-- Populate members
+INSERT INTO fit_member(first_name, last_name, email, signedup_date, username, mem_password, age) VALUES 
+('Alice', 'Smith', 'alice.smith@email.com', '2024-01-01', 'alicesmith', 'abc', 24),
+('Bob', 'Johnson', 'bob.johnson@email.com', '2024-01-02', 'bobjohnson', 'abc', 20);
+
+INSERT INTO health_metrics(mem_id, weight, height, bmi, body_fat_perc) VALUES 
+(1, 72.50, 1.60, 28.32, 20.20),
+(2, 85.00, 1.78, 26.83, 22.00);
+
+INSERT INTO fitness_goal(mem_id, goal, st_date, end_date) VALUES 
+(1, 'Building muscle', '2024-01-03', '2024-12-01'),
+(2, 'Shedding fat', '2024-01-05', '2024-06-01');
+
+INSERT INTO health_stat(steps, oxygen_intake, heart_rate, blood_pressure) VALUES 
+(10000, 35.80, 120, 130),
+(2000, 36.00, 100, 120);
+
+INSERT INTO exercise_routine(mem_id, health_stat_id, exercise, duration, target) VALUES 
+(1, 1, 'crunches, plank, leg-lift, flutter kick, mountain climber', 30, 'Core'),
+(2, 2, 'squat, deadlift, bench press, rows, pull ups', 45, 'Strength');
+
+INSERT INTO fitness_ach(mem_id, fit_goal_id, complete, comp_date) VALUES 
+(1, 1, false, NULL),
+(2, 2, false, NULL);
+
+
+-- Populate trainers
+INSERT INTO trainer(first_name, last_name, email, username, trainer_password) VALUES
+('Charlie', 'Williams', 'charlie.williams@email.com', 'charliewilliams', 'abc'), 
+('David', 'Brown', 'david.brown@email.com', 'davidbrown', 'abc');
+
+INSERT INTO availability(trainer_id, avail_date, target, st_time, end_time) VALUES 
+(1, '2024-04-04', 'Pilates', '09:00:00', '11:00:00'),
+(1, '2024-04-06', 'Zumba', '12:00:00', '14:00:00'),
+(1, '2024-04-08', 'Spin','18:00:00', '20:00:00'),
+(2, '2024-04-05', 'Kickboxing', '14:00:00', '16:00:00'),
+(2, '2024-04-07', 'Tai Chi', '15:00:00', '17:00:00'),
+(2, '2024-04-09', 'Yoga', '08:00:00', '10:00:00');
+
+-- Populate admin
+
+INSERT INTO fit_admin(first_name, last_name, email, username, admin_password) VALUES 
+('Eva', 'Jones', 'eva.jones@email.com', 'evajones', 'abc'), 
+('Frank', 'Davis', 'frank.davis@email.com', 'frankdavis', 'abc');
+
+INSERT INTO room(room_name) VALUES
+('Studio A'),
+('Studio B'),
+('Studio C'),
+('Studio D');
+
+INSERT INTO room_booking(trainer_id, avail_id, room_num, st_time, end_time, booking_date, capacity, curr_size, price) VALUES 
+(1, 1, 1, '09:00:00', '11:00:00', '2024-04-04', 1, 0, 60.00),
+(1, 2, 2, '12:00:00', '14:00:00', '2024-04-06', 20, 2, 25.99),
+(2, 4, 3, '14:00:00', '16:00:00', '2024-04-05', 15, 0, 30.00),
+(2, 6, 4, '08:00:00', '10:00:00', '2024-04-09', 30, 1, 20.00);
+
+INSERT INTO fit_session(mem_id, trainer_id, room_book_id, target) VALUES
+(1, 1, 2, 'Zumba'),
+(2, 1, 2, 'Zumba'),
+(2, 2, 4, 'Yoga');
+
+INSERT INTO maintenance_booking()
---
+++ SQL/Tables.sql
@@ -0,0 +1,175 @@
+-- Member tables
+create table fit_member (
+	mem_id 				serial, 
+	first_name			text not null,
+	last_name			text not null, 
+	email               text not null unique,
+	signedup_date       date,
+	username            text not null unique,
+	mem_password        text not null,
+	age					int,
+	bill				int DEFAULT 50,
+	primary key (mem_id)
+);
+
+
+create table health_metrics (
+	health_met_id	serial, 
+	mem_id			integer not null,
+	weight			decimal(5,2),
+	height 			decimal(5,2),
+	bmi				decimal(5,2),
+	body_fat_perc	decimal(5,2),
+	foreign key (mem_id) references fit_member
+		on delete CASCADE,
+	primary key (health_met_id)
+);
+
+create table fitness_goal (
+	fit_goal_id 	serial,
+	mem_id			integer not null,
+	goal 			text,
+	st_date 		date,
+	end_date		date,
+	foreign key (mem_id) references fit_member
+		on delete CASCADE,
+	primary key (fit_goal_id)
+);
+
+create table health_stat (
+	health_stat_id		serial,
+	steps 				int,
+	oxygen_intake		decimal(5,2),
+	heart_rate			int,
+	blood_pressure		int,
+	primary key (health_stat_id)
+);
+
+create table exercise_routine (
+	exer_rout_id	serial,
+	mem_id			integer not null,
+	health_stat_id	integer not null,
+	exercise		text,
+	duration		int,
+	target			text,
+	
+	foreign key (mem_id) references fit_member
+		on delete CASCADE,
+	foreign key (health_stat_id) references health_stat
+		on delete CASCADE,
+	primary key (exer_rout_id)
+);
+
+create table fitness_ach (
+	fit_ach_id		serial,
+	mem_id			integer not null,
+	fit_goal_id		integer not null,
+	complete 		boolean,
+	comp_date		date,
+	
+	
+	foreign key (mem_id) references fit_member
+		on delete CASCADE,
+	foreign key (fit_goal_id) references fitness_goal
+		on delete CASCADE,
+	primary key (fit_ach_id)
+);
+
+
+
+-- trainer table
+create table trainer (
+	trainer_id			serial,
+	first_name			text not null,
+	last_name			text not null, 
+	email               text not null unique,
+	username            text not null unique,
+	trainer_password    text not null,
+	primary key (trainer_id)
+);
+
+
+create table availability (
+	avail_id			serial,
+	trainer_id 			integer not null,
+	avail_date 			date,
+	target              text,
+	st_time				time,
+	end_time			time,
+	
+	
+	foreign key (trainer_id) references trainer
+		on delete CASCADE,
+	primary key (avail_id)
+);
+
+
+-- Admin tables
+create table fit_admin (
+	admin_id 			serial,
+	first_name			text not null,
+	last_name			text not null, 
+	email               text not null unique,
+	username            text not null unique,
+	admin_password      text not null,
+	primary key (admin_id)
+);
+
+create table room (
+    room_id             serial,
+    room_name           text,    
+    primary key (room_id)
+);
+
+
+create table room_booking (
+	room_book_id	serial,
+	trainer_id		integer not null,
+	avail_id        int,
+	room_num		int,
+	st_time			time,
+	end_time		time,
+	booking_date	date,
+	capacity		int,
+	curr_size		int check(curr_size <= capacity),
+	price           decimal(10, 2),
+	
+	foreign key (avail_id) references availability
+		on delete CASCADE,
+	foreign key (trainer_id) references trainer
+		on delete CASCADE,
+	foreign key (room_num) references room
+		on delete CASCADE,
+	primary key (room_book_id)
+);
+
+
+create table maintenance_booking (
+	fit_equip_id		serial,
+	admin_id		integer not null,
+	room_id			integer not null,
+	main_date		date,
+	
+	primary key (fit_equip_id),
+	foreign key (admin_id) references fit_admin
+		on delete CASCADE,
+	foreign key (room_id) references room
+		on delete CASCADE
+);
+
+
+create table fit_session (
+	sess_id			serial,
+	mem_id			integer not null,
+	trainer_id		integer not null,
+	room_book_id	integer not null,
+	target			text,
+	
+	primary key(sess_id),
+	foreign key (mem_id) references fit_member
+		on delete CASCADE,
+	foreign key (trainer_id) references trainer
+		on delete CASCADE,
+	foreign key (room_book_id) references room_booking
+		on delete CASCADE
+);
